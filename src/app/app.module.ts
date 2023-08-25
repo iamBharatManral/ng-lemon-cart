90fa922d40ba623f6f598ec114754dd2e78e2b53
iamBharatManral <justbharatmanral@gmail.com>
2023-08-25 19:16:11 +0530

    feat: add user, manager, pos, inventory module with home component ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManagerModule } from './manager/manager.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PosModule } from './pos/pos.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
